4f1ab45dfb6b8f641616318e6c8ca4dcc67aa01c
Martin Caron <martincaron@macbook-pro-de-martin-2.home>
2019-11-09 11:46:22 +0100

    Menu+MainLayout+MainSkeleton+Dataclass
    DONE
    Main layout with fragment implementation, menu and tabs
    linked actions and functions to buttons in tab
    TODO
    fragments, recyclerview for fragment list

--- app/src/main/java/com/ismin/opendataapp/MainActivity.kt
+++ app/src/main/java/com/ismin/opendataapp/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.ismin.opendataapp
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private val starbucksTable: ArrayList<Starbucks> = arrayListOf();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.layout)
+        setContentView(R.layout.activity_main)
+
+        displayStarbucksList(); //On commence par afficher la liste des starbucks dans l'applicatipon
+
+        this.findViewById<Button>(R.id.a_starbucks_list).setOnClickListener{
+            displayStarbucksList()
+        }
+        this.findViewById<Button>(R.id.a_app_details).setOnClickListener{
+            displayAppDetails()
+        }
+        this.findViewById<Button>(R.id.a_map).setOnClickListener{
+            displayMap()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun displayStarbucksList(){
+
+    }
+
+    fun displayAppDetails(){
+
+    }
+
+    fun displayMap(){
+
     }
 }
---
+++ app/src/main/java/com/ismin/opendataapp/Starbucks.kt
@@ -0,0 +1,4 @@
+package com.ismin.opendataapp
+
+data class Starbucks(val city:String) {
+}
